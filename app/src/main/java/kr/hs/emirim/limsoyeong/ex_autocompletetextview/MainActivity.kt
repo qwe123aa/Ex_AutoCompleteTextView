e8c357cc48f797d4f21196342bb29ef23842f7a5
thdud__ <love05061114@gmail.com>
2023-08-03 19:04:41 +0900

    AutoCompleteTextView

--- app/src/main/java/kr/hs/emirim/limsoyeong/ex_autocompletetextview/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/limsoyeong/ex_autocompletetextview/MainActivity.kt
@@ -2,6 +2,9 @@ package kr.hs.emirim.limsoyeong.ex_autocompletetextview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.MultiAutoCompleteTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var items = arrayOf("CSI-뉴욕", "CSI-라스베가스", "CSI-마이애미", "Friends", "Fringe", "Lost", "Lost World")
-        
+
+        var autoText = findViewById<AutoCompleteTextView>(R.id.autoText)
+        var multipleText = findViewById<MultiAutoCompleteTextView>(R.id.multiAutooText)
+
+        var adapter = ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line, items)
+        autoText.setAdapter(adapter)
+
+        var token = MultiAutoCompleteTextView.CommaTokenizer()
+        multipleText.setTokenizer(token)
+        multipleText.setAdapter(adapter)
+
+
     }
 }
