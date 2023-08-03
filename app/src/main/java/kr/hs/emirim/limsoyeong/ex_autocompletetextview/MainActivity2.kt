01bcb14c2c737491a6c7ef45490ef7cf67512ef8
thdud__ <love05061114@gmail.com>
2023-08-03 19:25:58 +0900

    ProgressBar, SeekBar, RatingBar 연습

---
+++ app/src/main/java/kr/hs/emirim/limsoyeong/ex_autocompletetextview/MainActivity2.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.limsoyeong.ex_autocompletetextview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
